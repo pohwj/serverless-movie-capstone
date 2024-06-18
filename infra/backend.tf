e55a9fa0c7b55275834656350bc8dffeb85619eb
wenjie <pohwj24@gmail.com>
2024-06-18 15:51:22 +0800

    include new tf file

---
+++ infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "movies-tf-state-bucket"
+    key            = "infra/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "movies_tf_state_lock"  
+  }
+}
